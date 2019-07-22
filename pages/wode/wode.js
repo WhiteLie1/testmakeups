761d1a875fc957ba63b521b8a913f85678b9b118
WhiteLie1 <1136462446@qq.com>
2019-07-22 21:13:17 +0800

    初步的界面规划
    除了微聊界面没有实现，其他的都做了初略的处理

---
+++ pages/wode/wode.js
@@ -0,0 +1,136 @@
+// pages/wode/wode.js
+//获取应用实例
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //获取微信头像的设置
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    //我的菜单的设置
+    menuitems: [
+      { text: '完善信息', url: '../userinfo/userinfo', icon: '../../images/WanShanXinxi.png', tips: '' },
+      { text: '个性设置', url: '../userinfo/userinfo', icon: '../../image/GeXinSheZhi.png', tips: '' },
+      { text: '收货地址', url: '../userinfo/userinfo', icon: '../../images/ShouHuoDiZhi.png', tips: '' },
+      { text: '全部订单', url: '../userinfo/userinfo', icon: '../../image/QuanBuDingDan.png', tips: '' }
+    ],
+    //刷新的设置
+    pageNum: 1,       // 设置加载的第几次，默认是第一次
+    isFirstLoad: true,   // 用于判断List数组是不是空数组，默认true，空的数组
+    hasMore: false,    // “加载更多”
+  },
+  // 下拉刷新
+  onPullDownRefresh: function () {
+    // 显示导航栏loading
+    wx.showNavigationBarLoading();
+    // 调用接口加载数据
+    this.loadData();
+    // 隐藏导航栏loading
+    wx.hideNavigationBarLoading();
+    // 当处理完数据刷新后，wx.stopPullDownRefresh可以停止当前页面的下拉刷新
+    wx.stopPullDownRefresh();
+  },
+  // 上拉加载
+  onReachBottom(e) {
+    let that = this;
+    if (that.data.hasMore) {
+      that.setData({
+        pageNum: that.data.pageNum + 1,  // 每次触发上拉事件，把pageNum+1
+        isFirstLoad: false                // 触发到上拉事件，把isFirstLoad设为为false
+      });
+
+      that.loadData();
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    var that = this;
+    if (app.globalData.userInfo) {
+      that.setUserInfo(app.globalData.userInfo);
+    } else if (that.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        that.setUserInfo(res.userInfo);
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          that.setUserInfo(res.userInfo);
+        }
+      })
+    }
+  },
+
+  getUserInfo: function (e) {
+    this.setUserInfo(e.detail.userInfo);
+  },
+
+  setUserInfo: function (userInfo) {
+    if (userInfo != null) {
+      app.globalData.userInfo = userInfo
+      this.setData({
+        userInfo: userInfo,
+        hasUserInfo: true
+      })
+    }
+  },
+
+
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
